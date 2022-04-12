184ef4eaa175797343c6a11cc2a8f46410c989e4
Erbolot Zhoomartov <e.zhoomartov@agimagroup.ru>
2022-04-12 21:29:24 +0600

    Initial Commit

---
+++ CombinePedProject/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CombinePedProject
+//  Copyright (c) 2022 Heads and Hands. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
